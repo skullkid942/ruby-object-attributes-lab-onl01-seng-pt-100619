b4617136d21032b2d90e79ecc32d749d1cae9df8
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-23 06:23:55 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  def initialize(dog_name, breed)
+    @name = dog_name
+    @breed = breed
+  end 
+  
+  def name=(dog_name)
+    @name = dog_name
+  end 
+  
+  def name
+    self.dog_name
+  end 
+  
+  def breed 
+    self.breed 
+  end 
+end 
