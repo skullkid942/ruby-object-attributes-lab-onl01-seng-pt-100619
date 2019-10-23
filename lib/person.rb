f76f9c9ffd6c9b8b3d119e4051d21628ceb779a0
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-23 13:47:21 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+  def initialize(name, job)
+    @name = person_name
+    @job  = person_job
+  end 
+  
+  def name=(person_name)
+    @name = person_name
+  end 
+  
+  def name
+    person_name
+  end
+  
+  def job=(person_job)
+    @job = person_job
+  end 
+  
+  def job
+    person_job
+  end 
+end 
+  
